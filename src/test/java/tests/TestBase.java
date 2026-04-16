3bced0ff7d7ce26e508c762b2b17b141fd13642a
Oleg Razumov <orazumoff@gmail.com>
2026-04-16 16:41:10 +0300

    create new branch, add parametrs for selenoide

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -8,30 +8,38 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class TestBase {
 
-    @BeforeAll
-    public static void setUp() {
+   @BeforeAll
+    static void setUp() {
 
-        WebDriverManager.chromedriver().setup();
-
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://www.sberleasing.ru";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.baseUrl = System.getProperty("baseUrl");  //https://www.sberleasing.ru
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 60000;
         Configuration.headless = false;
 
-        Configuration.browserCapabilities.setCapability("goog:chromeOptions",
-                new org.openqa.selenium.chrome.ChromeOptions()
-                        .addArguments("--remote-allow-origins=*")
-                        .addArguments("--disable-dev-shm-usage")
-                        .addArguments("--no-sandbox")
-        );
+        String loginSelenoid = System.getProperty("loginSelenoid");
+        String passwordSelenoid = System.getProperty("passwordSelenoid");
+        String urlSelenoid = System.getProperty("urlSelenoid");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://" + loginSelenoid + ":" + passwordSelenoid + "@" + urlSelenoid;
     }
 
     @BeforeEach
