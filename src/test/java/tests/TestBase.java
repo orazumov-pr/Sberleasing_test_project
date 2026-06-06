a7563d1bcd62f787d77d626640d61c45d89ff1ed
Oleg Razumov <orazumoff@gmail.com>
2026-06-06 19:40:19 +0300

    change TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,37 +2,52 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
 
-    @BeforeAll
-    public static void setUp() {
-        // AllureSelenide listener - сам делает скриншоты и сохраняет HTML
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+     @BeforeAll
+    static void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+
+                SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(true)
                 .includeSelenideSteps(true)
         );
 
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://www.sberleasing.ru";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.baseUrl = System.getProperty("baseUrl");  //https://www.sberleasing.ru
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
+
+        String loginSelenoid = System.getProperty("loginSelenoid");
+        String passwordSelenoid = System.getProperty("passwordSelenoid");
+        String urlSelenoid = System.getProperty("urlSelenoid");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://" + loginSelenoid + ":" + passwordSelenoid + "@" + urlSelenoid;
+
+        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
-        Configuration.pageLoadTimeout = 60000;
         Configuration.headless = false;
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        Configuration.browserCapabilities.setCapability("goog:chromeOptions", options);
     }
+
 
     @BeforeEach
     public void beforeEach() {
